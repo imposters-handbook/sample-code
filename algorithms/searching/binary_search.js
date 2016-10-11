3844c7e511546040693da8df5a4436ae90f7ddf2
Rob Conery <robconery@gmail.com>
2016-10-11 14:58:58 -0700

    Added binary search

---
+++ algorithms/searching/binary_search.js
@@ -0,0 +1,31 @@
+var list = [1,2,3,4,5,6];
+
+function binarySearch(list, lookFor) {
+  var min = 0, max = list.length -1;
+  var middle;
+
+  while (min <= max) {
+    //find the middle of the list
+    middle = Math.floor((min + max) / 2);
+
+    //if we just happen to land on it...
+    if (list[middle] === lookFor) {
+      return middle;
+    }
+    else {
+      //the list is sorted, so if we're looking too low...
+      if (list[middle] < lookFor) {
+        //increase the mininum
+        min = guess + 1;
+      }
+      else {
+        //decrease the max
+        max = guess - 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch(list,3));
